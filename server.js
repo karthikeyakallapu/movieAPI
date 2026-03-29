2ace2802f8d18ce871667a41366cfb7058473df6
Karthikeya Kallapu <karthikeyakallapu@gmail.com>
2026-03-29 21:37:44 +0530

    updated npm

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const fs = require("fs");
-const PORT = 3100;
-const data = fs.readFileSync("./GlobalMovies.json");
-const movies = JSON.parse(data);
-app.use(express.static("public"));
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3100;
+const publicDirPath = path.join(__dirname, "public");
+const movies = require("./GlobalMovies.json");
+
+app.use(express.static(publicDirPath));
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("<h2>Welcome to Karthikeya's Movie API</h2>")
-})
+    res.send("<h2>Welcome to Karthikeya's Movie API</h2>");
+});
+
 app.get("/movies", (request, response) => {
     response.send(movies);
 });
 
-
-
 app.get("/movies/:element", (request, response) => {
     const movieId = request.params.element;
-    const movie = movies.find((movie) => movie[movieId]);
+    const movie = movies.find((movieItem) => movieItem[movieId]);
+
     if (movie) {
         response.send(movie[movieId]);
-    } else {
-        response.status(404).send("Movie not found");
+        return;
     }
+
+    response.status(404).send("Movie not found");
 });
 
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+}
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+module.exports = app;
